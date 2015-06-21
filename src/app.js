ac6b38ea98bfa9fdd771412c358aa97338d511c7
GwonHyeok <kh4975@naver.com>
2015-06-22 01:21:32 +0900

    버튼 타이틀 애니메이션 딜레이 추가

--- src/app.js
+++ src/app.js
@@ -8,16 +8,15 @@ var AppMainScene = cc.Scene.extend({
         var titleNodeAction = ccs.actionTimelineCache.createAction("res/main/node_title.json");
         var buttonGrounNodeAction = ccs.actionTimelineCache.createAction("res/main/layer_button_group.json");
 
-        node.runAction(firNodeAction);
         node.runAction(titleNodeAction);
         node.runAction(buttonGrounNodeAction);
-        firNodeAction.gotoFrameAndPlay(0, 120, true);
-        titleNodeAction.gotoFrameAndPlay(0, 50, false);
-        buttonGrounNodeAction.gotoFrameAndPlay(0, 100, false);
+        firNodeAction.gotoFrameAndPlay(0, 100, true);
+        titleNodeAction.gotoFrameAndPlay(0, 100, false);
+        buttonGrounNodeAction.gotoFrameAndPlay(0, 200, false);
 
         var tapToStartNode = node.getChildByName("node_text");
-        var fadeAnimation = cc.fadeTo(0.5, 127.5);
-        var fadeOutAnimation = cc.fadeTo(0.5, 255);
+        var fadeAnimation = cc.fadeTo(1, 127.5);
+        var fadeOutAnimation = cc.fadeTo(1, 255);
         var pulseAnimation = new cc.Sequence(fadeAnimation, fadeOutAnimation);
         tapToStartNode.runAction(cc.RepeatForever(pulseAnimation));
     }
